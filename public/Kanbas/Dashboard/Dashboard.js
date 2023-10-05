823053546f2963d5edb013757096e228aa05e859
ninap <paprocki.n@northeastern.edu>
2023-10-05 17:32:13 -0400

    first commit in a2

---
+++ public/Kanbas/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+function Dashboard() {
+    const courses = [
+      {
+        _id: "123",
+        number: "CS4550",
+        title: "Web Development",
+        description: "Learn web development",
+        students: [],
+        modules: [],
+        assignmets: []
+    }
+]
+}
